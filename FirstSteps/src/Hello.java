99bb62ed7200ea49ff76789d26e63a408a3e9fd9
Yusuf Şengün <yusuffsengun@gmail.com>
2021-06-15 21:07:20 +0300

    First Steps

---
+++ FirstSteps/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int myFirstNumber = (10 + 5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+        System.out.println(myTotal);
+    }
+}
