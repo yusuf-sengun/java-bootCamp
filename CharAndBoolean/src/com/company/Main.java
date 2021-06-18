a30cabdeca4593b5c6d658d2186b709523f08851
Yusuf Şengün <yusuffsengun@gmail.com>
2021-06-18 22:33:40 +0300

    Char and Boolean data types

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        System.out.println(myChar);
+        char myCopyRigthChar = '\u00A9';
+        System.out.println(myCopyRigthChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        System.out.println(isCustomerOverTwentyOne);
+
+    }
+}
