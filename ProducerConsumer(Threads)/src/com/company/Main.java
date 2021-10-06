3dae7c9f68080f80b59299687e491dd63a064fdc
Yusuf Şengün <yusuffsengun@gmail.com>
2021-10-06 11:55:19 +0300

    ProducerConsumer (Threads)

---
+++ ProducerConsumer(Threads)/src/com/company/Main.java
@@ -0,0 +1,109 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayBlockingQueue<String> buffer = new ArrayBlockingQueue<String>(6);
+
+        ExecutorService executorService
+                = Executors.newFixedThreadPool(5);
+
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_BLUE);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_GREEN);
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_RED + "I'm being printed for the callable");
+                return "This is the callable result";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            System.out.println("Thread running the task was interrupted");
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong");
+        }
+
+        executorService.shutdown();
+    }
+}
+
+class MyProducer implements Runnable {
+    private ArrayBlockingQueue<String> buffer;
+    private String color;
+
+    public MyProducer(ArrayBlockingQueue<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        String[] nums = {"1", "2", "3", "4", "5"};
+        for (String num : nums) {
+            try {
+                System.out.println(color + "Adding.. " + num);
+                buffer.put(num);
+                Thread.sleep(random.nextInt(1000));
+            } catch (InterruptedException e) {
+                System.out.println("Producer was interrupted");
+            }
+        }
+        System.out.println(color + "Adding EOF and exiting...");
+        try {
+            buffer.put("EOF");
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
+
+class MyConsumer implements Runnable {
+    private ArrayBlockingQueue<String> buffer;
+    private String color;
+
+    public MyConsumer(ArrayBlockingQueue<String> buffer, String color) {
+        this.buffer = buffer;
+        this.color = color;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (buffer) {
+                try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
+                    if (buffer.peek().equalsIgnoreCase("EOF")) {
+                        System.out.println(color + "Exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "Removed " + buffer.take());
+                    }
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+
+
+    }
+
+}
+
+
