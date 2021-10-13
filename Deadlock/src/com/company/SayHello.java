37cfcdaf397321d811049c63080b4fdb07b0769e
Yusuf Şengün <yusuffsengun@gmail.com>
2021-10-13 09:21:50 +0300

    Deadlock

---
+++ Deadlock/src/com/company/SayHello.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class SayHello {
+    public static void main(String[] args) {
+        PolitePerson jane = new PolitePerson("Jane");
+        PolitePerson john = new PolitePerson("John");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                jane.sayHello(john);
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                john.sayHello(jane);
+            }
+        }).start();
+
+    }
+    static  class PolitePerson {
+        private final String name;
+
+        public PolitePerson(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public synchronized void sayHello(PolitePerson person){
+            System.out.format("%s: %s"+ " has said hello to me! %n",this.name,person.getName());
+            person.sayHelloBack(this);
+        }
+        public synchronized void sayHelloBack(PolitePerson person){
+            System.out.format("%s: %s"+ " has said hello back to me! %n",this.name,person.getName());
+        }
+
+    }
+}
